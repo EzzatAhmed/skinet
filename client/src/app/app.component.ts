1c4c66cad02082782119fde37d300ed10b5855fe
unknown <ezzat_ahmed96@hotmail.com>
2021-05-06 12:06:50 +0200

    End Of Section 8

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { IPagination } from './models/pagination';
+import { IProduct } from './models/product';
+import { HttpClient } from '@angular/common/http';
+import { Component,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'SkiNet';
+export class AppComponent implements OnInit{
+  title = 'Skinet';
+  products: IProduct[] | undefined;
+
+
+  constructor(private http: HttpClient){  }
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe(
+      (response: any) => {
+      this.products=response.data;
+    },error => {
+      console.log(error);
+    });
+  }
 }
